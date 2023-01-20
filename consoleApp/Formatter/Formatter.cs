a37ba46dc68413d7e16305b6792674c1279fe2f9
Julien Blais <sharpshark13@gmail.com>
2023-01-19 23:28:49 -0500

    Added basic formatting code.

---
+++ consoleApp/Formatter/Formatter.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace STAR.Format
+{
+    public class Formatter
+    {
+        List<Action<StringBuilder>> m_Rules = new List<Action<StringBuilder>>();
+
+        public void AddRule(Action<StringBuilder> rule)
+        {
+            m_Rules.Add(rule);
+        }
+
+        public void Format(StringBuilder str)
+        {
+            foreach (var rule in m_Rules)
+                rule.Invoke(str);
+
+        }
+
+        public string Format(string str)
+        {
+            var stringBuilder = new StringBuilder(str);
+            Format(stringBuilder);
+            return stringBuilder.ToString();
+        }
+    }
+
+    public static class FormatterBuilder
+    {
+        public static Formatter With(this Formatter formatter, Action<StringBuilder> rule)
+        {
+            formatter.AddRule(rule);
+            return formatter;
+        }
+    }
+}
---
+++ consoleApp/Formatter/Rules.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace STAR.Format
+{
+    public static class Rules
+    {
+        public static void RemoveItalics(StringBuilder builder)
+        {
+
+        }
+    }
+}
