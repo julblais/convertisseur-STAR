a47ff3babd1eb8f5fa754bbe3e4177ce6d851000
Julien Blais <sharpshark13@gmail.com>
2023-01-21 17:32:33 -0500

    Added formatting context.

---
+++ consoleApp/Formatter/FormattingContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace STAR.Format
+{
+    public class FormattingContext
+    {
+        public readonly StringBuilder contents;
+
+        public FormattingContext(StringBuilder contents)
+        {
+            this.contents = contents;
+        }
+    }
+}
--- consoleApp/Formatter/Rules.cs
+++ consoleApp/Formatter/Rules.cs
@@ -1,15 +1,16 @@
-﻿using System.Text;
+﻿using consoleApp.Formatter;
+using System.Text;
 
 namespace STAR.Format
 {
     public static class Rules
     {
-        public static void RemoveItalics(StringBuilder builder)
+        public static void RemoveItalics(FormattingContext context)
         {
 
         }
 
-        public static void RemoveSpaces(StringBuilder builder)
+        public static void RemoveSpaces(FormattingContext context)
         {
 
         }
