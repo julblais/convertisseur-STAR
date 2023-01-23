5a273a1e036703a87f4886acb31819745fe38ab6
Julien Blais <sharpshark13@gmail.com>
2023-01-23 00:10:34 -0500

    Added utilities.

---
+++ consoleApp/Formatter/RulesHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace STAR.Format
+{
+    static class RulesHelpers
+    {
+        public static SplitResult SplitInTwo(this ReadOnlySpan<char> text, ReadOnlySpan<char> delimiter)
+        {
+            var result = text.IndexOf(delimiter);
+            if (result != -1) //found
+            {
+                var first = text.Slice(0, result);
+                var last = text.Slice(result + delimiter.Length);
+                return new SplitResult(first, last);
+            }
+
+            return SplitResult.CreateEmpty();
+        }
+    }
+}
