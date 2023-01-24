462ef3b53747cffd511974db204e9465fee5f292
Julien Blais <sharpshark13@gmail.com>
2023-01-23 20:32:37 -0500

    Added command context.

---
+++ consoleApp/Formatter/CommandContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace STAR.Format
+{
+    public readonly struct CommandContext
+    {
+        public readonly string originalContent;
+        public readonly IEnumerable<Command> input;
+        public readonly ICollection<Command> output;
+
+        public CommandContext(string content, IEnumerable<Command> input)
+        {
+            originalContent = content;
+            this.input = input;
+            output = new List<Command>();
+        }
+
+        public readonly void Add(Command command)
+        {
+            output.Add(command);
+        }
+    }
+}
