6f0c551327cf0af8617908dd5c73f8c6fe62e7ac
Julien Blais <sharpshark13@gmail.com>
2023-01-30 21:03:20 -0500

    Updated version to 0.5.4

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -8,7 +8,7 @@ namespace STAR.ConsoleApp
 {
     internal class Program
     {
-        const string version = "0.5.3";
+        const string version = "0.5.4";
 
         const string ArgHelp = "--help";
         const string ArgCodePage = "--codepage";
