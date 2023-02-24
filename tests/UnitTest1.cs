fcd153f372fb2116f4241c9efa5880bda058c729
Julien Blais <sharpshark13@gmail.com>
2023-02-23 22:44:05 -0500

    Added basic test case struct.

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,20 +1,44 @@
+using System;
 using NUnit.Framework;
-using STAR.Writer;
+using STAR.Format;
 
-namespace STAR.Tests 
+namespace STAR.Tests
 {
-    public class Tests 
+    readonly struct TestCase
     {
+        public readonly Formatter.Rule[] rules { get; init; }
+        public readonly string fileName { get; init; }
+
+        public TestCase(string fileName, params Formatter.Rule[] rules)
+        {
+            this.fileName = fileName;
+            this.rules = rules;
+        }
+    }
+
+    class Tests
+    {
+        static TestCase[] fileCases = new TestCase[]
+        {
+            new TestCase("testFile.lst", Rules.FixEndline)
+        };
+
         [SetUp]
-        public void Setup() 
+        public void Setup()
         {
         }
 
         [Test]
-        public void Test1() 
+        public void Test1()
         {
-            var wordWriter = new WordWriter("test");
             Assert.Pass();
+        }
+
+        [TestCaseSource(nameof(fileCases))]
+        public void Test2(TestCase testCase)
+        {
+            Console.WriteLine(testCase.fileName);
+            Console.WriteLine(testCase.rules);
         }
     }
 }
