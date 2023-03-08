36794e7ae5975222849d48e87b6a336ddf19c63d
Julien Blais <sharpshark13@gmail.com>
2023-03-07 19:45:10 -0500

    Updated versions.

--- core/Version.cs
+++ core/Version.cs
@@ -4,7 +4,7 @@ namespace STAR
     {
         public const int Major = 0;
         public const int Minor = 7;
-        public const int Patch = 1;
+        public const int Patch = 2;
 
         public static readonly string AsString = $"{Major}.{Minor}.{Patch}";
     }
--- webApp/Version.cs
+++ webApp/Version.cs
@@ -4,7 +4,7 @@ namespace webApp
     {
         public const int Major = 0;
         public const int Minor = 8;
-        public const int Patch = 0;
+        public const int Patch = 1;
 
         public static readonly string AsString = $"{Major}.{Minor}.{Patch}";
         public static readonly string Core = STAR.Version.AsString;
